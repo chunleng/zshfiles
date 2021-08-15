3481bac0468fb76f08da41b9f71ce37760a2fd66
Lim Chun Leng <lim@logly.co.jp>
2021-08-15 18:49:04 +0900

    Change job scheduler to jobber

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,12 +10,8 @@ grep -q $CURRENT_ZSH /etc/shells || echo $CURRENT_ZSH | sudo tee -a /etc/shells 
 
 ln -sfn ${APP_PATH}/zshrc ${HOME}/.zshrc
 
-# Install cron
-launchctl unload ${APP_PATH}/launchd/local.brew.update.plist
-launchctl load ${APP_PATH}/launchd/local.brew.update.plist
-launchctl unload ${APP_PATH}/launchd/local.pip.update.plist
-launchctl load ${APP_PATH}/launchd/local.pip.update.plist
-launchctl unload ${APP_PATH}/launchd/local.docker.prune.plist
-launchctl load ${APP_PATH}/launchd/local.docker.prune.plist
-launchctl unload ${APP_PATH}/launchd/local.gcloud.update.plist
-launchctl load ${APP_PATH}/launchd/local.gcloud.update.plist
+# Install jobber
+mkdir -p ${HOME}/.jobber-logs
+ln -sfn ${APP_PATH}/jobber/jobber.yaml ${HOME}/.jobber
+ln -sfn ${APP_PATH}/jobber/notify.sh ${HOME}/.jobber-notify.sh
+jobber reload
