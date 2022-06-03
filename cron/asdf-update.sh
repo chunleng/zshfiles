583a5c8d5143ad72e6c4cbe18bbb7f9e72c5c7e1
Lim Chun Leng <limchunleng@gmail.com>
2022-06-03 23:26:36 +0900

    add support for auto update asdf-neovim ref:master (for apple silicon)

--- cron/asdf-update.sh
+++ cron/asdf-update.sh
@@ -2,5 +2,6 @@
 
 set -eu
 
+asdf uninstall neovim ref:master && asdf install neovim ref:master
 asdf uninstall neovim nightly && asdf install neovim nightly
 asdf uninstall nodejs lts-gallium && asdf install nodejs lts-gallium
