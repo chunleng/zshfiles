81fd9e012ca397841f051bfe6341414af7e8842e
Lim Chun Leng <limchunleng@gmail.com>
2022-08-03 20:24:51 +0900

    allow resources in asdf-update to be useable during update or when update fails

--- cron/asdf-update.sh
+++ cron/asdf-update.sh
@@ -2,6 +2,15 @@
 
 set -eu
 
-asdf uninstall neovim ref:master && asdf install neovim ref:master
-asdf uninstall neovim nightly && asdf install neovim nightly
-asdf uninstall nodejs lts-gallium && asdf install nodejs lts-gallium
+asdf install neovim nightly && \
+  rm -rf ~/.asdf/installs/neovim/nightly.bak && \
+  mv ~/.asdf/installs/neovim/nightly{,.bak} && \
+  asdf global neovim nightly.bak && \
+  asdf install neovim nightly && \
+  asdf global neovim nightly
+asdf install nodejs lts-gallium && \
+  rm -rf ~/.asdf/installs/nodejs/lts-gallium.bak && \
+  mv ~/.asdf/installs/nodejs/lts-gallium{,.bak} && \
+  asdf global nodejs lts-gallium.bak && \
+  asdf install nodejs lts-gallium && \
+  asdf global nodejs lts-gallium
