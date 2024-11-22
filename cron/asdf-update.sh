c9d65a1446b8213f40994d2e853ff6aa8ddac571
Lim Chun Leng <limchunleng@gmail.com>
2024-11-22 19:47:44 +0900

    chore: remove update neovim as I no longer use nightly

--- cron/asdf-update.sh
+++ cron/asdf-update.sh
@@ -3,9 +3,3 @@
 set -eu
 
 asdf plugin-update --all
-asdf install neovim nightly && \
-  rm -rf ~/.asdf/installs/neovim/nightly.bak && \
-  mv ~/.asdf/installs/neovim/nightly{,.bak} && \
-  asdf global neovim nightly.bak && \
-  asdf install neovim nightly && \
-  asdf global neovim nightly
