637ef3effb5e661bf7be1d651a78dbe35cdfe674
Lim Chun Leng <limchunleng@gmail.com>
2025-07-25 12:18:48 +0900

    chore: remove BTTOrder from backup-btt.sh

--- cron/backup-btt.sh
+++ cron/backup-btt.sh
@@ -8,5 +8,5 @@ osascript -e "tell application \"BetterTouchTool\"
 	export_preset \"Default\" outputPath \"${file_to_update}\" with includeSettings
 end tell"
 
-jq -S 'del(.. | objects | .BTTPresetUUID?, .BTTLastUpdatedAt, .BTTGeneralSettings) | walk(if type == "array" then sort else . end)' "${file_to_update}" > "${file_to_update}.tmp"
+jq -S 'del(.. | objects | .BTTOrder, .BTTPresetUUID?, .BTTLastUpdatedAt, .BTTGeneralSettings) | walk(if type == "array" then sort else . end)' "${file_to_update}" > "${file_to_update}.tmp"
 mv "${file_to_update}.tmp" "${file_to_update}"
